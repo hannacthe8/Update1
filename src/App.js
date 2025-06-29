9827e750588550b1eca0b8d9415558e8ba57129d
hannacthe8 <hannac@the8camerons.ca>
2025-06-29 19:54:56 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Specials from './pages/Specials';
+import About from './pages/About';
+import Admin from './pages/Admin';
+import Cart from './pages/Cart';
+import './App.css';
 
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/specials" element={<Specials />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
